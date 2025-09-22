61a414add883d7b6852592e4edcf730c3c847396
sokinpui <sosokinpui@gmail.com>
2025-09-22 16:00:54 +0800

    update script

--- worker.sh
+++ worker.sh
@@ -1,8 +1,31 @@
 #!/bin/zsh
 
+# if $1 is not a number exit
+
+arg="$1"
+
+if [ -n "$arg" ] && [ "$arg" -eq "$arg" ] 2>/dev/null; then
+else
+  echo "Exiting..."
+  echo "number of workers should be integer"
+  exit 0
+fi
+
+if [ "$arg" -ne "$arg" ]; then
+fi
+
+if [ -z $1 ]; then
+  echo "1 worker starting..."
+else
+  echo "$1 workers starting.."
+fi
+
+
+set -e
+
 export http_proxy=http://127.0.0.1:1087;export https_proxy=http://127.0.0.1:1087;export ALL_PROXY=socks5://127.0.0.1:1080
 
-for i in {1..10}; do
+for i in {1.."$arg"}; do
   ./bin/worker &
   worker_pids+=($!)
 done
