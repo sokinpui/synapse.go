cdef9b0fc1ad4210f5b1a494a60089ba3ee7ae2f
sokinpui <sosokinpui@gmail.com>
2025-09-22 14:50:55 +0800

    add worker script

---
+++ worker.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+export http_proxy=http://127.0.0.1:1087;export https_proxy=http://127.0.0.1:1087;export ALL_PROXY=socks5://127.0.0.1:1080
+./bin/worker &
+./bin/worker &
+./bin/worker &
+./bin/worker &
+./bin/worker &
+./bin/worker &
+./bin/worker &
+./bin/worker &
+./bin/worker &
+./bin/worker &
